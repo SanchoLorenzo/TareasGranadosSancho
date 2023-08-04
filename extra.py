cfec1e24fca82f29f6098bbedaccfd61e730193f
SanchoLorenzo <lorenzo7sancho@gmail.com>
2023-08-03 19:01:16 -0600

    Subi el archivo extra con errores

---
+++ extra.py
@@ -0,0 +1,9 @@
+def hola_mundo(nombre, edad, comida)    :
+
+
+
+    return ("Hola mi nombre es " + nombre + ", tengo "
+            + edad + " años " "y me gusta comer " + comida + ".")
+
+
+print(    hola_mundo("Kaleb", "19", "arroz"))
